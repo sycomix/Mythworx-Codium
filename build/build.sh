b233c3980700b10c4dd5319a5775010b61a3f65c
CI <-ci@not-real.com>
2024-02-10 08:45:54 -0600

    yay more idjut fixes

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@
 # to run with Bash: "C:\Program Files\Git\bin\bash.exe" ./build/build.sh
 ###
 
-export APP_NAME="VSCodium"
+export APP_NAME="MythWorx"
 export CI_BUILD="no"
 export SHOULD_BUILD="yes"
 export SKIP_ASSETS="yes"
